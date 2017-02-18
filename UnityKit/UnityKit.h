49c05b8a501816ba076cfa66c2a35c75c4eaac43
Jak Tiano <jtiano@magicleap.com>
2017-02-18 15:58:23 -0500

    Initial Commit

---
+++ UnityKit/UnityKit.h
@@ -0,0 +1,19 @@
+//
+//  UnityKit.h
+//  UnityKit
+//
+//  Created by Jak Tiano on 2/18/17.
+//  Copyright © 2017 Jak Tiano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnityKit.
+FOUNDATION_EXPORT double UnityKitVersionNumber;
+
+//! Project version string for UnityKit.
+FOUNDATION_EXPORT const unsigned char UnityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnityKit/PublicHeader.h>
+
+
