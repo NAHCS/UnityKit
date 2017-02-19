e90c745d5711ab52a65b016133cd18262815e71f
Jak Tiano <jtiano@magicleap.com>
2017-02-19 16:56:47 -0500

    Added Quick and Nimble testing.

--- UnityKitTests/UnityKitTests.swift
+++ UnityKitTests/UnityKitTests.swift
@@ -6,22 +6,18 @@
 //  Copyright © 2017 Jak Tiano. All rights reserved.
 //
 
-import XCTest
-@testable import UnityKit
+import Quick
+import Nimble
+import UnityKit
 
-class UnityKitTests: XCTestCase {
+class UnityKitTests: QuickSpec {
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func spec () {
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testExample() {
-
+        describe("true") {
+            it("is true") {
+                expect(true).to(beTruthy())
+            }
+        }
     }
 }
